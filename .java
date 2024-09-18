ba70519351fa74d504ceb0f3d90362fe8b87c583
Aromal.sp <150415644+Aromalsp123@users.noreply.github.com>
2024-09-18 22:06:37 +0530

    Create .java

---
+++ .java
@@ -0,0 +1,16 @@
+public class demo {
+        public static void main(String [] args){
+         int num[] = {5 ,6,9,11,13}
+          int target = 11;
+         int result = linearSearch(nums, target );
+         sytem.out.println("Element found at index : " + result );
+ 
+         }
+       public static int linearSearch(int[] nums,int target ){
+       return -1;
+              
+
+  
+    }   
+
+}
